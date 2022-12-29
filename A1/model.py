a55867f6ef62a79cebf8e1ba1d7c54fb72ed36a0
polo <65348980+golfscript@users.noreply.github.com>
2022-12-29 13:11:03 +0000

    Implementation feature extration using PCA

--- A1/model.py
+++ A1/model.py
@@ -1,7 +1,18 @@
 import numpy as np
+from matplotlib import pyplot as plt
 from skimage import io
+from sklearn.decomposition import PCA
 
 def load_image(filename):
     img = io.imread(filename, as_gray=True) # use built-in grayscale loading
     crop = img[90:180, 54:124] # crop to face
     return crop
+
+def feature_extraction(X, test=False):
+    global pca
+    Xf = X.reshape(len(X),-1) # use this to flatten the last dims of X without copying
+    if not test:
+        pca = PCA(n_components=100).fit(Xf)
+        plt.plot(pca.explained_variance_ratio_.cumsum())
+        plt.show()
+    return pca.transform(Xf)
