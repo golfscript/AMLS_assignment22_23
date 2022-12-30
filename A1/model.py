443bc7861c3338327f8aeac96aa2be74050a5663
polo <65348980+golfscript@users.noreply.github.com>
2022-12-30 10:26:42 +0000

    Move feature extraction into fit method

--- A1/model.py
+++ A1/model.py
@@ -11,22 +11,19 @@ def load_image(filename):
     crop = img[90:180, 54:124] # crop to face
     return exposure.equalize_hist(crop)
 
-def feature_extraction(X, test=False):
-    global pca
-    Xf = X.reshape(len(X),-1) # use this to flatten the last dims of X without copying
-    if not test:
-        pca = PCA(n_components=100).fit(Xf)
-        plt.plot(pca.explained_variance_ratio_.cumsum())
-        plt.show()
-    return pca.transform(Xf)
-
 def fit(X, y):
-    global model
+    global pca,model
+    Xf = X.reshape(len(X),-1) # use this to flatten the last dims of X without copying
+    pca = PCA(n_components=100).fit(Xf)
+    plt.plot(pca.explained_variance_ratio_.cumsum())
+    plt.show()
+    Xp = pca.transform(Xf)
     param_grid = {'kernel':['rbf','sigmoid'], 'C':[0.5, 1, 2, 5, 10], 'gamma':[0.005, 0.01, 0.02, 0.05, 0.1]}
-    model = HalvingGridSearchCV(SVC(), param_grid, verbose=3).fit(X, y)
+    model = HalvingGridSearchCV(SVC(), param_grid, verbose=3).fit(Xp, y)
     print(model.best_params_)
     return model.best_score_
 
 def predict(X):
-    global model
-    return model.predict(X)
+    global pca,model
+    Xf = X.reshape(len(X),-1)
+    return model.predict(pca.transform(Xf))
