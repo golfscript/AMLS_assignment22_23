e6ac91f4bfcf81709001f605c69032fc10fe92e1
polo <65348980+golfscript@users.noreply.github.com>
2022-12-30 16:22:34 +0000

    Switch to PIL for image loading as much faster

--- B1/model.py
+++ B1/model.py
@@ -1,25 +1,26 @@
 import numpy as np
 from matplotlib import pyplot as plt
-from skimage import io, transform
+from PIL import Image, ImageOps
 from sklearn import tree
 
 RND = 1 # use in all functions that need random seed in order to ensure repeatability
 
 def load_image(filename):
-    img = io.imread(filename, as_gray=True) # use built-in grayscale loading
-    crop = img[160:400,150:350] # crop to face
-    return transform.rescale(crop,0.5) # rescale to half size
+    img = ImageOps.grayscale(Image.open(filename)) # use built-in grayscale conversion
+    crop = img.crop((150,160,350,400)) # crop to face
+    scale = ImageOps.scale(crop,1/3,Image.NEAREST) # rescale to one third size
+    return np.asarray(scale)/255.0 # convert to range [0,1]
 
 def fit(X, y):
     global model
-    X = X.reshape(len(X),-1) # use this to flatten the last dims of X without copying
+    X = X.reshape(len(X),-1) # flatten the last dims of X without copying
     model = tree.DecisionTreeClassifier(max_depth=8, random_state=RND).fit(X, y)
     plt.figure(figsize=(12,10))
-    tree.plot_tree(model, fontsize=10) # plot the decision tree
+    tree.plot_tree(model, fontsize=9) # plot the decision tree
     plt.show()
-    return model.score(X,y)
+    return model
 
 def predict(X):
     global model
-    X = X.reshape(len(X),-1) # use this to flatten the last dims of X without copying
+    X = X.reshape(len(X),-1) # flatten the last dims of X without copying
     return model.predict(X) # select prediction with highest output
