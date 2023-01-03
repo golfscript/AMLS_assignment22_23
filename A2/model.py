16fcedb492a0276886e2518222ae21d05a9edcd1
polo <65348980+golfscript@users.noreply.github.com>
2023-01-03 20:31:06 +0000

    Simplify tf model for A2

--- A2/model.py
+++ A2/model.py
@@ -3,7 +3,6 @@ import tensorflow as tf
 import cv2
 
 RND = 1 # use in all functions that need random seed in order to ensure repeatability
-tf.keras.utils.set_random_seed(RND)
 
 def load_image(filename):
     img = cv2.imread(filename, cv2.IMREAD_GRAYSCALE) # use built-in grayscale conversion
@@ -14,23 +13,23 @@ def fit(X, y, X_test=None, y_test=None):
     global model
     if X.ndim == 3:
         X = X.reshape(*X.shape,1) # reshape if necessary for Conv2D layer
+    tf.keras.utils.set_random_seed(RND)
+    classes = y.max()+1 # generalise to work with any number of classed
     model = tf.keras.Sequential([
         tf.keras.layers.Rescaling(1./255),
+        tf.keras.layers.Conv2D(3, 3, activation='relu'),
+        tf.keras.layers.MaxPooling2D(),
         tf.keras.layers.Conv2D(4, 3, activation='relu'),
         tf.keras.layers.MaxPooling2D(),
-        tf.keras.layers.Conv2D(5, 3, activation='relu'),
-        tf.keras.layers.MaxPooling2D(),
         tf.keras.layers.Flatten(),
-        tf.keras.layers.Dense(2)
+        tf.keras.layers.Dense(classes)
         ])
     model.compile(
       optimizer='adam',
       loss=tf.keras.losses.SparseCategoricalCrossentropy(from_logits=True),
       metrics=['accuracy'])
     
-    model.fit(X, y, epochs=5, validation_data=(X_test, y_test))
-
-    return model.evaluate(X,y)
+    return model.fit(X, y, epochs=6, validation_data=(X_test, y_test)).history['accuracy'][-1]
 
 def predict(X):
     global model
