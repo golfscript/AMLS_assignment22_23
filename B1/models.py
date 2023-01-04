829925a90492f12bf3995c89adf4cdea17b93a27
polo <65348980+golfscript@users.noreply.github.com>
2023-01-04 11:53:56 +0000

    Switch to multi-model for tasks B1,B2

--- B1/models.py
+++ B1/models.py
@@ -6,21 +6,33 @@ from sklearn import tree
 RND = 1 # use in all functions that need random seed in order to ensure repeatability
 
 def load_image(filename):
-    img = cv2.imread(filename, cv2.IMREAD_GRAYSCALE) # use built-in grayscale conversion
-    crop = img[160:400,150:350] # crop to face
-    scale = cv2.resize(crop,None,fx=1/2,fy=1/2,interpolation=cv2.INTER_NEAREST_EXACT) # rescale to one third size
-    return scale
+  img = cv2.imread(filename, cv2.IMREAD_GRAYSCALE) # use built-in grayscale conversion
+  crop = img[160:400,150:350] # crop to face
+  scale = cv2.resize(crop,None,fx=1/2,fy=1/2,interpolation=cv2.INTER_NEAREST_EXACT) # rescale to one third size
+  return scale
 
-def fit(X, y):
-    global model
-    X = X.reshape(len(X),-1) # flatten the last dims of X without copying
-    model = tree.DecisionTreeClassifier(max_depth=8, random_state=RND).fit(X, y)
-    plt.figure(figsize=(12,10))
-    tree.plot_tree(model, fontsize=9) # plot the decision tree
-    plt.show()
-    return model.score(X,y)
+def _prepare(X):
+  return X.reshape(len(X),-1) # flatten the last dims of X without copying
 
-def predict(X):
-    global model
-    X = X.reshape(len(X),-1) # flatten the last dims of X without copying
-    return model.predict(X) # select prediction with highest output
+def _plot(model):
+  plt.figure(figsize=(12,10))
+  tree.plot_tree(model, fontsize=9) # plot the decision tree
+  plt.show()
+
+class DecisionTree:
+  def __init__(self, max_depth):
+    self.max_depth = max_depth
+
+  def fit(self, X, y):
+    X = _prepare(X)
+    print('Peforming Decision Tree Classification')
+    self.model = tree.DecisionTreeClassifier(max_depth=8, random_state=RND).fit(X, y)
+    _plot(self.model)
+    return self.model.score(X,y)
+
+def predict(self, X):
+    X = _prepare(X)
+    return self.model.predict(X) # select prediction with highest output
+
+models = {'Best: Decision Tree with max depth of 8': DecisionTree(8),
+          'Decision Tree with max depth of 7': DecisionTree(7)}
--- B2/models.py
+++ B2/models.py
@@ -6,20 +6,32 @@ from sklearn import tree
 RND = 1 # use in all functions that need random seed in order to ensure repeatability
 
 def load_image(filename):
-    img = cv2.imread(filename) # this task needs colour
-    crop = img[240:280,160:340,::-1] # crop to eyes, reverse GBR to RGB
-    return crop
+  img = cv2.imread(filename) # this task needs colour
+  crop = img[240:280,160:340,::-1] # crop to eyes, reverse GBR to RGB
+  return crop
 
-def fit(X, y):
-    global model
-    X = X.reshape(len(X),-1) # flatten the last dims of X without copying
-    model = tree.DecisionTreeClassifier(max_depth=8, random_state=RND).fit(X, y)
-    plt.figure(figsize=(12,10))
-    tree.plot_tree(model, fontsize=9) # plot the decision tree
-    plt.show()
-    return model.score(X,y)
+def _prepare(X):
+  return X.reshape(len(X),-1) # flatten the last dims of X without copying
 
-def predict(X):
-    global model
-    X = X.reshape(len(X),-1) # flatten the last dims of X without copying
-    return model.predict(X) # select prediction with highest output
+def _plot(model):
+  plt.figure(figsize=(12,10))
+  tree.plot_tree(model, fontsize=9) # plot the decision tree
+  plt.show()
+
+class DecisionTree:
+  def __init__(self, max_depth):
+    self.max_depth = max_depth
+
+  def fit(self, X, y):
+    X = _prepare(X)
+    print('Peforming Decision Tree Classification')
+    self.model = tree.DecisionTreeClassifier(max_depth=8, random_state=RND).fit(X, y)
+    _plot(self.model)
+    return self.model.score(X,y)
+
+def predict(self, X):
+    X = _prepare(X)
+    return self.model.predict(X) # select prediction with highest output
+
+models = {'Best: Decision Tree with max depth of 8': DecisionTree(8),
+          'Decision Tree with max depth of 7': DecisionTree(7)}
