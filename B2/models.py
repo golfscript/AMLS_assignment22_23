80767c1086f707bc9d94cd4da22c14f2f036ac8e
polo <65348980+golfscript@users.noreply.github.com>
2023-01-07 12:50:24 +0000

    Implement GridSearchCV on B2

--- B2/models.py
+++ B2/models.py
@@ -1,7 +1,8 @@
 import numpy as np
 from matplotlib import pyplot as plt
 import cv2
-from sklearn import tree
+from sklearn.tree import DecisionTreeClassifier, plot_tree
+from sklearn.model_selection import GridSearchCV
 
 RND = 1 # use in all functions that need random seed in order to ensure repeatability
 
@@ -15,37 +16,37 @@ def _prepare(X):
 
 def _plot(model):
   plt.figure(figsize=(12,10))
-  tree.plot_tree(model, fontsize=9) # plot the decision tree
+  plot_tree(model, fontsize=9) # plot the decision tree
   plt.show()
 
 def _crop_enhance(X):
-  print('Peforming extreme cropping and contrast streching...')
-  X = X[:,25,30:45].astype(np.uint16)
+  print('Peforming Iris Cropping and Contrast Enhancement...')
+  X = X[:,25,30:45] # crop to iris
   # Contrast stretching
   mn = X.min(axis=1,keepdims=True)
   mx = X.max(axis=1,keepdims=True)
   print(f'{(mn==mx).all(axis=-1).mean():.2%} of samples have no variance')
-  return 255*(X-mn)//np.maximum((mx-mn),1)
+  return np.divide(X-mn,np.maximum((mx-mn),1), dtype=np.float32)*255 # Tree classifier creates copy if not float32
 
-class DecisionTree:
-  def __init__(self, max_depth, crop_enhance):
-    self.model = tree.DecisionTreeClassifier(max_depth=max_depth, random_state=RND)
+class DTreeCV:
+  def __init__(self, crop_enhance):
     self.crop_enhance = crop_enhance
-
+  
   def fit(self, X, y):
-    if self.crop_enhance: X = _crop_enhance(X) 
+    if self.crop_enhance: X = _crop_enhance(X)
     X = _prepare(X)
-    print('Peforming Decision Tree Fitting...')
-    self.model.fit(X, y)
-    _plot(self.model)
+    min_depth = y.max() # need at least this many nodes to distinguish all classes
+    params = {'criterion':['gini', 'entropy'], 'max_depth':range(min_depth, min_depth+6)}
+    self.model = GridSearchCV(DecisionTreeClassifier(random_state=RND), params, verbose=3).fit(X,y)
+    print("Optimal hyper-parameters:", self.model.best_params_)
+    print("Mean cross-validated accuracy:", self.model.best_score_)
+    _plot(self.model.best_estimator_)
     return self.model.score(X,y)
 
   def predict(self, X):
     if self.crop_enhance: X = _crop_enhance(X) 
     X = _prepare(X)
-    return self.model.predict(X) # select prediction with highest output
+    return self.model.predict(X)
 
-options = {'*Best B2: Crop, Enhance & Decision Tree max depth of 4': DecisionTree(4, True),
-          'Decision Tree max depth of 8': DecisionTree(8, False),
-          'Decision Tree max depth of 7': DecisionTree(7, False),
-          'Decision Tree max depth of 6': DecisionTree(6, False)}
+options = {'*Best B2: Iris Crop/Enhance & Decision Tree with CV': DTreeCV(True),
+          'Decision Tree with CV': DTreeCV(False)}
