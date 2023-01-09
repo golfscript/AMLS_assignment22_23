2e0c40ecc74b577e1face6bfa9c7638d2cf58ee8
polo <65348980+golfscript@users.noreply.github.com>
2023-01-09 19:15:42 +0000

    Use CV to optimise params in B1

--- B1/models.py
+++ B1/models.py
@@ -1,7 +1,9 @@
 import numpy as np
 from matplotlib import pyplot as plt
+from tqdm.notebook import tqdm
 import cv2
-from sklearn import tree
+from sklearn.tree import DecisionTreeClassifier, plot_tree
+from sklearn.model_selection import cross_val_score
 
 RND = 1 # use in all functions that need random seed in order to ensure repeatability
 
@@ -16,16 +18,31 @@ def _prepare(X):
 
 def _plot(model):
   plt.figure(figsize=(12,10))
-  tree.plot_tree(model, fontsize=9) # plot the decision tree
+  plot_tree(model, fontsize=9) # plot the decision tree
   plt.show()
 
 class DecisionTree:
-  def __init__(self, max_depth):
-    self.model = tree.DecisionTreeClassifier(max_depth=max_depth, random_state=RND)
+  def __init__(self):
+    self.model = DecisionTreeClassifier(random_state=RND)
 
   def fit(self, X, y):
     X = _prepare(X)
-    print('Peforming Decision Tree Fitting...')
+    min_depth = y.max() # need at least this many nodes to distinguish all classes
+    self.model.set_params(max_depth=min_depth)
+    params = {'criterion':['gini', 'entropy'], 'max_depth':range(min_depth, min_depth+6)}
+    
+    for param, values in params.items():
+      print(f'Peforming Cross Validation on optimal {param}...')
+      prog_bar = tqdm(values, desc='cross validation')
+      scores = [cross_val_score(self.model.set_params(**{param:v}), X, y, n_jobs=-1).mean() for v in prog_bar]
+      plt.plot([str(v) for v in values], scores)
+      plt.show()
+
+      best = values[np.argmax(scores)]
+      print(f'Optimal {param} is', best)
+      self.model.set_params(**{param:best})
+
+    print('Performing final fit on all data with optimal params...')
     self.model.fit(X, y)
     _plot(self.model)
     return self.model.score(X,y)
@@ -34,7 +51,4 @@ class DecisionTree:
     X = _prepare(X)
     return self.model.predict(X)
 
-options = {'*Best B1: Decision Tree with max depth of 6': DecisionTree(6),
-          'Decision Tree with max depth of 5': DecisionTree(5),
-          'Decision Tree with max depth of 4': DecisionTree(4),
-          'Decision Tree with max depth of 3': DecisionTree(3)}
+options = {'*Best B1: Decision Tree with CV of optimal params': DecisionTree()}
