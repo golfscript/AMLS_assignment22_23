70606c1d00ce2d52effd961184d777cd5964b32c
polo <65348980+golfscript@users.noreply.github.com>
2023-01-09 12:46:38 +0000

    Use pipeline for task A1

--- A1/models.py
+++ A1/models.py
@@ -4,9 +4,11 @@ from matplotlib.ticker import PercentFormatter
 import cv2
 from skimage import exposure
 from sklearn.decomposition import PCA
+from sklearn.preprocessing import StandardScaler
 from sklearn.svm import SVC
 from sklearn.experimental import enable_halving_search_cv # need this to enable line below
 from sklearn.model_selection import HalvingGridSearchCV
+from sklearn.pipeline import make_pipeline
 
 RND = 1 # use in all functions that need random seed in order to ensure repeatability
 
@@ -15,17 +17,18 @@ def load_image(filename):
   crop = img[90:180, 54:124] # crop to face
   return crop
 
-def _clahe(X): # Contrast limited adaptive histogram equalisation
-  print("Performing Contrast Limited Adaptive Histogram Equalisation", end='')
-  for i in range(len(X)):
-    X[i] = exposure.equalize_adapthist(X[i])
-    if i%100 == 0: print('.', end='')
-  print()
-
 def _prepare(X, clahe, augment):
   Z = np.empty((2 if augment else 1,*X.shape), dtype=np.float32) # limit to float32 to save space
-  Z[0] = X/255. # convert to [0,1]
-  if clahe: _clahe(Z[0])
+  if clahe:
+    print("Performing Contrast Limited Adaptive Histogram Equalisation", end='')
+    eq = cv2.createCLAHE(clipLimit=4.0, tileGridSize=(5,6)) # choose grid that divides 70*90 image
+    for i in range(len(X)):
+      Z[0][i] = eq.apply(X[i])/255.
+      if i%100 == 0: print('.', end='')
+    print()
+  else:
+    Z[0] = X/255. # just convert to [0,1]
+
   if augment:
     print('Performing Data Augmentation...')
     Z[1] = Z[0,:,:,::-1]  # copy images, but reverse left to right
@@ -47,32 +50,29 @@ def _plot(pca):
   plt.show()
 
 class base:
-  def __init__(self, pca_components=120, clahe=True, augment=True):
-    self.pca = PCA(n_components=pca_components, random_state=RND)
+  def __init__(self, pca_components, clahe=True, augment=True):
+    self.model = make_pipeline(PCA(n_components=pca_components,random_state=RND), StandardScaler(), SVC())
     self.clahe = clahe
     self.augment = augment
 
   def predict(self, X):
     X = _prepare(X, self.clahe, False)
-    X = self.pca.transform(X)
     return self.model.predict(X)
 
 class PCA_SVC_HCV(base):
   def fit(self, X, y):
     X = _prepare(X, self.clahe, self.augment)
     if self.augment: y = np.tile(y,2) # duplicate labels
-
-    print('Performing Principle Component Analysis...')
-    X = self.pca.fit_transform(X) # do fit & transform in one step as quicker
-    _plot(self.pca)
-
+    
     print('Performing Halving Grid Search with Cross Validation...')
-    Cs = [0.5, 1, 2, 5, 10, 20]
-    param_grid = [{'kernel':['linear','poly'], 'C':Cs},
-                  {'kernel': ['rbf','sigmoid'], 'C':Cs, 'gamma':[0.001, 0.003, 0.01, 0.03, 0.1]}]
-    self.model = HalvingGridSearchCV(SVC(), param_grid, max_resources=len(X)//2, random_state=RND, verbose=3).fit(X, y)
-    print("Optimal hyper-parameters:", self.model.best_params_)
-    print("Mean cross-validated accuracy:", self.model.best_score_)
+    Cs = [0.01, 0.1, 1]
+    param_grid = [{'pca__n_components':[100,120,140], 'svc__kernel':['linear','poly'], 'svc__C':Cs},
+                  {'pca__n_components':[100,120,140], 'svc__kernel': ['rbf','sigmoid'], 'svc__C':Cs, 'svc__gamma':[0.001, 0.01, 0.1]}]
+    hcv = HalvingGridSearchCV(self.model, param_grid, random_state=RND, verbose=3, refit=False).fit(X[:5000], y[:5000])
+    print("Optimal hyper-parameters:", hcv.best_params_)
+    print("Mean cross-validated accuracy:", hcv.best_score_)
+    self.model.set_params(**hcv.best_params_) # set pipeline to best params
+    self.model.fit(X,y) # fit to augmented data
     return self.model.score(X,y)
 
 class PCA_SVC(base):
@@ -80,17 +80,15 @@ class PCA_SVC(base):
     X = _prepare(X, self.clahe, self.augment)
     if self.augment: y = np.tile(y,2) # duplicate labels
 
-    print('Performing Principle Component Analysis...')
-    X = self.pca.fit_transform(X) # do fit & transform in one step as quicker
-    _plot(self.pca)
-
     print('Performing Support Vector Classification Fit...')
-    self.model = SVC().fit(X,y)
+    self.model.fit(X,y)
     return self.model.score(X,y)
 
-options = {'*Best A1: Clahe, Augment, PCA(120) & SVC Halving Grid Search with Cross Validation': PCA_SVC_HCV(120),
+options = {'*Best A1: Clahe, Augment, PCA & SVC Halving Grid Search with Cross Validation': PCA_SVC_HCV(),
           'Clahe, Augment, PCA(120) & SVC(default params)': PCA_SVC(120),
           'Clahe, Augment, PCA(100) & SVC with default params': PCA_SVC(100),
           'Clahe, Augment, PCA(140) & SVC with deafult params': PCA_SVC(140),
           'Augment, PCA(120) & SVC Halving Grid Search with Cross Validation': PCA_SVC_HCV(120, clahe=False),
-          'Augment, PCA(120) & SVC with default params': PCA_SVC(120, clahe=False)}
+          'Augment, PCA(120) & SVC with default params': PCA_SVC(120, clahe=False),
+          'PCA(120) & SVC with default params': PCA_SVC(120, clahe=False, augment=False),
+          'Augment, PCA(140) & SVC with default params': PCA_SVC(140, clahe=False)}
